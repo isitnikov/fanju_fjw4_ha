d5a5dc8ccbb28ecc20001070dfdf1e54d4a9ae8c
Ihor Sytnykov <576294+isitnikov@users.noreply.github.com>
2026-02-13 10:18:18 -0600

    Add configuration flow for FanJu FJW4 integration

---
+++ custom_components/fanju_fjw4/config_flow.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+
+import voluptuous as vol
+
+from homeassistant import config_entries
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
+
+from .api import FanJuApi, FanJuAuthError
+from .const import DOMAIN, CONF_USERNAME, CONF_PASSWORD
+
+
+class FanJuConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+        errors = {}
+
+        if user_input:
+            # Перевірка логіну одразу, щоб не плодити “мертві” інтеграції
+            session = async_get_clientsession(self.hass)
+            api = FanJuApi(session, user_input[CONF_USERNAME], user_input[CONF_PASSWORD])
+            try:
+                await api.ensure_token()
+            except FanJuAuthError:
+                errors["base"] = "auth_failed"
+            except Exception:
+                errors["base"] = "unknown"
+            else:
+                return self.async_create_entry(title="FanJu FJW4", data=user_input)
+
+        schema = vol.Schema({
+            vol.Required(CONF_USERNAME): str,
+            vol.Required(CONF_PASSWORD): str,
+        })
+        return self.async_show_form(step_id="user", data_schema=schema, errors=errors)
