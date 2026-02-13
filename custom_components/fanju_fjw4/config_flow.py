7ccc9942c5e66fd9f743f03d5e2f21326eb62d7a
Ihor Sytnykov <576294+isitnikov@users.noreply.github.com>
2026-02-13 10:25:50 -0600

    Refactor config flow and add options handling

--- custom_components/fanju_fjw4/config_flow.py
+++ custom_components/fanju_fjw4/config_flow.py
@@ -3,11 +3,17 @@ from __future__ import annotations
 import voluptuous as vol
 
 from homeassistant import config_entries
-from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import OptionsFlow
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
 from .api import FanJuApi, FanJuAuthError
-from .const import DOMAIN, CONF_USERNAME, CONF_PASSWORD
+from .const import (
+    DOMAIN,
+    CONF_USERNAME,
+    CONF_PASSWORD,
+    CONF_POLLING_INTERVAL,
+    DEFAULT_POLLING_INTERVAL,
+)
 
 
 class FanJuConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -16,10 +22,14 @@ class FanJuConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     async def async_step_user(self, user_input=None):
         errors = {}
 
-        if user_input:
-            # Перевірка логіну одразу, щоб не плодити “мертві” інтеграції
+        if user_input is not None:
             session = async_get_clientsession(self.hass)
-            api = FanJuApi(session, user_input[CONF_USERNAME], user_input[CONF_PASSWORD])
+            api = FanJuApi(
+                session,
+                user_input[CONF_USERNAME],
+                user_input[CONF_PASSWORD],
+            )
+
             try:
                 await api.ensure_token()
             except FanJuAuthError:
@@ -27,10 +37,52 @@ class FanJuConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             except Exception:
                 errors["base"] = "unknown"
             else:
-                return self.async_create_entry(title="FanJu FJW4", data=user_input)
+                return self.async_create_entry(
+                    title="FanJu FJW4 Weather Station",
+                    data={
+                        CONF_USERNAME: user_input[CONF_USERNAME],
+                        CONF_PASSWORD: user_input[CONF_PASSWORD],
+                    },
+                )
 
-        schema = vol.Schema({
-            vol.Required(CONF_USERNAME): str,
-            vol.Required(CONF_PASSWORD): str,
-        })
-        return self.async_show_form(step_id="user", data_schema=schema, errors=errors)
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema(
+                {
+                    vol.Required(CONF_USERNAME): str,
+                    vol.Required(CONF_PASSWORD): str,
+                }
+            ),
+            errors=errors,
+        )
+
+    @staticmethod
+    def async_get_options_flow(config_entry):
+        return FanJuOptionsFlowHandler(config_entry)
+
+
+class FanJuOptionsFlowHandler(OptionsFlow):
+    def __init__(self, config_entry):
+        self.config_entry = config_entry
+
+    async def async_step_init(self, user_input=None):
+        if user_input is not None:
+            return self.async_create_entry(title="", data=user_input)
+
+        return self.async_show_form(
+            step_id="init",
+            data_schema=vol.Schema(
+                {
+                    vol.Optional(
+                        CONF_POLLING_INTERVAL,
+                        default=self.config_entry.options.get(
+                            CONF_POLLING_INTERVAL,
+                            DEFAULT_POLLING_INTERVAL,
+                        ),
+                    ): vol.All(
+                        vol.Coerce(int),
+                        vol.Range(min=30, max=3600),
+                    ),
+                }
+            ),
+        )
